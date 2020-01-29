ba6ba6b000f2601d4c3e922d45f00e1d41c8da40
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-29 10:58:15 -1000

    Update runRAxML.sh

--- runRAxML.sh
+++ runRAxML.sh
@@ -7,5 +7,9 @@
 
 wd=/WORKDIR
 out=OUTPUT_NAME
+
+module load raxml
+module unload python
+module load python/3.6.1
 # Step 4) Run raxml
 raxmlHPC-PTHREADS -T 8 -f a -m PROTGAMMAJTT -N 100 -n $out -s $wd/supermatrix.aln.faa -p 13432 -x 89090
