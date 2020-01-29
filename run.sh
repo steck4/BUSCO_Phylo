1888fe98cedf916c5881bcf7bebd93b87c719f67
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-29 09:59:46 -1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,28 @@
 # Produce a phylogeny based on provided BUSCO protein and transcriptome runs.
-# 
 # USE THE SAME DATASET FOR EACH OF THEM
 #
-# This script is designed to run with the following tools
-#
-# It is recommanded to use the provided Dockerfile (see https://www.docker.com/what-docker)
-# 
+# This script is designed to run with the following tools:
 # - raxml 8
 # - mafft 7
 # - trimAl 1.4
 # - newick-utils 1.6
 # - BUSCO 3.0.2
 #
-# !!! > You need to run BUSCO on each species before running this script
+module unload python
+module load python/3.6.1
+module load busco
 #
-#wd=/workdir your working directory. If using docker, can be mounted as follow: docker run -v /yourfolder:/workdir ...
+# !!! You need to have run BUSCO on each species before running this script
+#
+wd=/WORKDIR #your working directory
 cd $wd
 mkdir extracted
 # Step 1) Extract BUSCO genes, produce one multispecies-fasta per BUSCO family
-# !!! >  You need to edit the script "extract_buscos_phylo.py" to provide information about each species folder
+# !!! You need to edit the script "extract_buscos_phylo.py" to provide information about each species folder
 echo "extracting BUSCO genes..."
 python3 /extract_buscos_phylo.py
 # Step 2) Run mafft and trimAl on each BUSCO family
-echo "mafft+trimAl..."
+echo "starting mafft trimAl"
 for b in $(ls $wd/extracted/*.faa)
 do
 echo $b
@@ -33,7 +33,8 @@ done
 python3 /superalignment.py $wd/extracted/
 mv $wd/extracted/supermatrix.aln.faa .
 # Step 4) Run raxml
-raxmlHPC-PTHREADS-SSE3 -T 8 -f a -m PROTGAMMAJTT -N 100 -n my_busco_phylo -s $wd/supermatrix.aln.faa -p 13432 -x 89090
+#raxmlHPC-PTHREADS-SSE3 -T 8 -f a -m PROTGAMMAJTT -N 100 -n my_busco_phylo -s $wd/supermatrix.aln.faa -p 13432 -x 89090
+qsub runRAxML.sh
 # Step 5) Root the tree
 # !!! > You need to replace OUTGRP by the name of your outgroup
-nw_reroot RAxML_bestTree.my_busco_phylo OUTGRP > final.nwk
+#nw_reroot RAxML_bestTree.my_busco_phylo OUTGRP > final.nwk
