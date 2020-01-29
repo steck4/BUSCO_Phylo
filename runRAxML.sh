8860102c824e5d83206ff1301a12ffd3b18851c7
steck4 <59516097+steck4@users.noreply.github.com>
2020-01-29 10:11:00 -1000

    Create runRAxML.sh

---
+++ runRAxML.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -k oe
+#PBS -m abe
+#PBS -M USER@hawaii.edu
+#PBS -N runRAxML
+#PBS -l nodes=2:ppn=16,vmem=200gb,walltime=10:00:00
+
+wd=/WORKDIR
+#out=OUTPUT_NAME
+# Step 4) Run raxml
+raxmlHPC-PTHREADS -T 8 -f a -m PROTGAMMAJTT -N 100 -n $out -s $wd/supermatrix.aln.faa -p 13432 -x 89090
